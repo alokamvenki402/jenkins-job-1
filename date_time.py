9ac03e6fc8dcbd9db62dcb4a2e2f2acbd267f230
Venkata Satyanarayana. Alokam <31393949+alokamvenki402@users.noreply.github.com>
2017-09-28 22:47:52 -0400

    Update date_time.py

--- date_time.py
+++ date_time.py
@@ -1,4 +1,20 @@
+import time
 import datetime
-now = datetime.datetime.now()
-print ("Current date and time : ")
-print (now.strftime("%Y-%m-%d %H:%M:%S"))
+def ss(D):
+    D_list = list(D)
+    if(D_list[1]==1):
+        return "st"
+    elif(D_list[1]==2):
+        return "nd"
+    elif(D_list[1]==3):
+        return "rd"
+    else:
+        return "th"
+def main():
+    Y = datetime.date.today().strftime("%Y")
+    M = datetime.date.today().strftime("%B")
+    D = datetime.date.today().strftime("%d")
+    HH = datetime.datetime.now().strftime("%I")
+    MM = datetime.datetime.now().strftime("%M")  
+    print("Printed on the",D.lstrip('0')+ss(D),"day of",M+",",Y,"at",HH+":"+MM+".")
+main()
